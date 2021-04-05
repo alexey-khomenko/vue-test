68e35484adffbff18f13a6d72d3dec9827e23c4e
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-04-05 22:38:44 +0300

    #22

--- src/App.vue
+++ src/App.vue
@@ -5,46 +5,42 @@
       <section>
         <div class="flex">
           <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700"
-            >Тикер</label
-            >
+            <label for="wallet" class="block text-sm font-medium text-gray-700">
+              Тикер
+            </label>
             <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                  v-model="ticker"
-                  @keydown.enter="add"
-                  type="text"
-                  name="wallet"
-                  id="wallet"
-                  class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                  placeholder="Например DOGE"
+              <input v-model="ticker"
+                     @keydown.enter="add"
+                     type="text"
+                     name="wallet"
+                     id="wallet"
+                     class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                     placeholder="Например DOGE"
               />
             </div>
           </div>
         </div>
-        <button
-            @click="add"
-            type="button"
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        <button type="button"
+                @click="add"
+                class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
           <!-- Heroicon name: solid/mail -->
-          <svg
-              class="-ml-0.5 mr-2 h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 24 24"
-              fill="#ffffff"
+          <svg class="-ml-0.5 mr-2 h-6 w-6"
+               xmlns="http://www.w3.org/2000/svg"
+               width="30"
+               height="30"
+               viewBox="0 0 24 24"
+               fill="#ffffff"
           >
             <path
-                d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
+                d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path>
           </svg>
           Добавить
         </button>
       </section>
 
       <template v-if="tickers.length">
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
         <div>
           <button
               class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
@@ -60,18 +56,15 @@
           >
             Вперед
           </button>
-          <div>Фильтр: <input v-model="filter" /></div>
+          <div>Фильтр: <input v-model="filter"/></div>
         </div>
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <div
-              v-for="t in paginatedTickers"
-              :key="t.name"
-              @click="select(t)"
-              :class="{
-              'border-4': selectedTicker === t
-            }"
-              class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+          <div v-for="t in paginatedTickers"
+               :key="t.name"
+               @click="select(t)"
+               :class="{'border-4': selectedTicker === t}"
+               class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
@@ -82,57 +75,53 @@
               </dd>
             </div>
             <div class="w-full border-t border-gray-200"></div>
-            <button
-                @click.stop="handleDelete(t)"
-                class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+            <button @click.stop="handleDelete(t)"
+                    class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
-              <svg
-                  class="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="#718096"
-                  aria-hidden="true"
+              <svg class="h-5 w-5"
+                   xmlns="http://www.w3.org/2000/svg"
+                   viewBox="0 0 20 20"
+                   fill="#718096"
+                   aria-hidden="true"
               >
-                <path
-                    fill-rule="evenodd"
-                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                    clip-rule="evenodd"
-                ></path></svg
-              >Удалить
+                <path fill-rule="evenodd"
+                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                      clip-rule="evenodd"
+                ></path>
+              </svg>
+              Удалить
             </button>
           </div>
         </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr class="w-full border-t border-gray-600 my-4"/>
       </template>
       <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
           {{ selectedTicker.name }} - USD
         </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div
-              v-for="(bar, idx) in normalizedGraph"
-              :key="idx"
-              :style="{ height: `${bar}%` }"
-              class="bg-purple-800 border w-10"
+        <div class="flex items-end border-gray-600 border-b border-l h-64"
+             ref="graph">
+          <div v-for="(bar, idx) in normalizedGraph"
+               :key="idx"
+               :style="{ height: `${bar}%` }"
+               class="bg-purple-800 border w-10"
           ></div>
         </div>
-        <button
-            @click="selectedTicker = null"
-            type="button"
-            class="absolute top-0 right-0"
+        <button @click="selectedTicker = null"
+                type="button"
+                class="absolute top-0 right-0"
         >
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              xmlns:svgjs="http://svgjs.com/svgjs"
-              version="1.1"
-              width="30"
-              height="30"
-              x="0"
-              y="0"
-              viewBox="0 0 511.76 511.76"
-              style="enable-background:new 0 0 512 512"
-              xml:space="preserve"
+          <svg xmlns="http://www.w3.org/2000/svg"
+               xmlns:xlink="http://www.w3.org/1999/xlink"
+               xmlns:svgjs="http://svgjs.com/svgjs"
+               version="1.1"
+               width="30"
+               height="30"
+               x="0"
+               y="0"
+               viewBox="0 0 511.76 511.76"
+               style="enable-background:new 0 0 512 512"
+               xml:space="preserve"
           >
             <g>
               <path
@@ -164,31 +153,32 @@
 // [x] График сломан если везде одинаковые значения
 // [x] При удалении тикера остается выбор
 
-import { subscribeToTicker, unsubscribeFromTicker } from "./api";
+import {subscribeToTicker, unsubscribeFromTicker} from './api';
 
 export default {
-  name: "App",
+  name: 'App',
 
   data() {
     return {
-      ticker: "",
-      filter: "",
+      ticker: '',
+      filter: '',
 
       tickers: [],
       selectedTicker: null,
 
       graph: [],
+      maxGraphElements: 1,
 
-      page: 1
+      page: 1,
     };
   },
 
   created() {
     const windowData = Object.fromEntries(
-        new URL(window.location).searchParams.entries()
+        new URL(window.location).searchParams.entries(),
     );
 
-    const VALID_KEYS = ["filter", "page"];
+    const VALID_KEYS = ['filter', 'page'];
 
     VALID_KEYS.forEach(key => {
       if (windowData[key]) {
@@ -204,18 +194,26 @@ export default {
     //   this.page = windowData.page;
     // }
 
-    const tickersData = localStorage.getItem("cryptonomicon-list");
+    const tickersData = localStorage.getItem('cryptonomicon-list');
 
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
       this.tickers.forEach(ticker => {
         subscribeToTicker(ticker.name, newPrice =>
-            this.updateTicker(ticker.name, newPrice)
+            this.updateTicker(ticker.name, newPrice),
         );
       });
     }
 
     setInterval(this.updateTickers, 5000);
+  },
+
+  mounted() {
+    window.addEventListener('resize', this.calculateMaxGraphElements);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener('resize', this.calculateMaxGraphElements);
   },
 
   computed: {
@@ -248,32 +246,44 @@ export default {
       }
 
       return this.graph.map(
-          price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+          price => 5 + ((price - minValue) * 95) / (maxValue - minValue),
       );
     },
 
     pageStateOptions() {
       return {
         filter: this.filter,
-        page: this.page
+        page: this.page,
       };
-    }
+    },
   },
 
   methods: {
+    calculateMaxGraphElements() {
+      if (!this.$refs.graph) {
+        return;
+      }
+
+      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
+    },
+
     updateTicker(tickerName, price) {
       this.tickers
           .filter(t => t.name === tickerName)
           .forEach(t => {
             if (t === this.selectedTicker) {
               this.graph.push(price);
+
+              while (this.graph.length > this.maxGraphElements) {
+                this.graph.shift();
+              }
             }
             t.price = price;
           });
     },
 
     formatPrice(price) {
-      if (price === "-") {
+      if (price === '-') {
         return price;
       }
       return price > 1 ? price.toFixed(2) : price.toPrecision(2);
@@ -282,14 +292,14 @@ export default {
     add() {
       const currentTicker = {
         name: this.ticker,
-        price: "-"
+        price: '-',
       };
 
       this.tickers = [...this.tickers, currentTicker];
-      this.ticker = "";
-      this.filter = "";
+      this.ticker = '';
+      this.filter = '';
       subscribeToTicker(currentTicker.name, newPrice =>
-          this.updateTicker(currentTicker.name, newPrice)
+          this.updateTicker(currentTicker.name, newPrice),
       );
     },
 
@@ -304,7 +314,7 @@ export default {
         this.selectedTicker = null;
       }
       unsubscribeFromTicker(tickerToRemove.name);
-    }
+    },
   },
 
   watch: {
@@ -315,7 +325,7 @@ export default {
     tickers(newValue, oldValue) {
       // Почему не сработал watch при добавлении?
       console.log(newValue === oldValue);
-      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
     },
 
     paginatedTickers() {
@@ -332,9 +342,9 @@ export default {
       window.history.pushState(
           null,
           document.title,
-          `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
+          `${window.location.pathname}?filter=${value.filter}&page=${value.page}`,
       );
-    }
-  }
+    },
+  },
 };
 </script>
